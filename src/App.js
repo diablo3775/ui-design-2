5621dd21ee4ad554598dfff107be26701f4211ad
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2022-09-02 19:58:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Cards from './pages/Cards/Cards';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Cards />
+      {/* <Cards /> */}
+      {/* <Cards />
+      <Cards />
+      <Cards /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/json/data.js
@@ -0,0 +1,151 @@
+
+import binance1 from '../../assets/binance/BinanceCoin(1).png'
+import binance2 from '../../assets/binance/BinanceCoinB.png'
+import binance from '../../assets/binance/BinanceCoin.png'
+import solana from '../../assets/solana/Solana.png'
+import solana1 from '../../assets/solana/Solana(1).png'
+import solana2 from '../../assets/solana/Solana(2).png'
+import solana3 from '../../assets/solana/Solana(3).png'
+import eth from '../../assets/eth/Ethereum.png'
+import eth1 from '../../assets/eth/Ethereum(1).png'
+import eth2 from '../../assets/eth/Ethereum(2).png'
+import eth3 from '../../assets/eth/Ethereum(3).png'
+import eth4 from '../../assets/eth/Ethereum(4).png'
+import eth5 from '../../assets/eth/Ethereum(5).png'
+import eth6 from '../../assets/eth/Ethereum(6).png'
+import dollargreen from '../../assets/dollargreen.png'
+import bitcoinlogo from '../../assets/Bitcoinlogo.png'
+import bitcointxt from '../../assets/Bitcointxt.png'
+import dollar1 from '../../assets/dollar1.png'
+import dollar2 from '../../assets/dollar2.png'
+import pp from '../../assets/popularpairs.png'
+import price from '../../assets/Price.png'
+import tvl from '../../assets/tvl.png'
+// 2nd Card
+import bitcoinsmall from '../../assets/card2/Bitcoin SV (BSV) (1).png'
+
+
+let data = [
+    {
+        dollargreen:dollargreen,
+        bitcoinlogo:bitcoinlogo,
+        bitcointxt:bitcointxt,
+        dollar1: dollar1,
+        dollar2: dollar2,
+        price: price,
+        tvl: tvl,
+        solana: solana,
+        solana1: solana1,
+        solana2: solana2,
+        solana3: solana3,
+        eth: eth,
+        eth1: eth1,
+        eth2: eth2,
+        eth3: eth3,
+        eth4: eth4,
+        eth5: eth5,
+        eth6: eth6,
+        binance2: binance2,
+        binance: binance,
+        binance1: binance1,
+        pp : pp
+    },
+    {
+        dollargreen:dollargreen,
+        bitcoinlogo:bitcoinlogo,
+        bitcointxt:bitcointxt,
+        dollar1: dollar1,
+        dollar2: dollar2,
+        price: price,
+        tvl: tvl,
+        solana: solana,
+        solana1: bitcoinsmall,
+        solana2: bitcoinsmall,
+        solana3: bitcoinsmall,
+        eth: eth,
+        eth1: eth1,
+        eth2: eth2,
+        eth3: eth3,
+        eth4: eth4,
+        eth5: eth5,
+        eth6: eth6,
+        binance2: binance2,
+        binance: binance,
+        binance1: binance1,
+        pp : pp
+    },
+    {
+        dollargreen:dollargreen,
+        bitcoinlogo:bitcoinlogo,
+        bitcointxt:bitcointxt,
+        dollar1: dollar1,
+        dollar2: dollar2,
+        price: price,
+        tvl: tvl,
+        solana: solana,
+        solana1: solana1,
+        solana2: solana2,
+        solana3: solana3,
+        eth: eth,
+        eth1: eth1,
+        eth2: eth2,
+        eth3: eth3,
+        eth4: eth4,
+        eth5: eth5,
+        eth6: eth6,
+        binance2: binance2,
+        binance: binance,
+        binance1: binance1,
+        pp : pp
+    },
+    {
+        dollargreen:dollargreen,
+        bitcoinlogo:bitcoinlogo,
+        bitcointxt:bitcointxt,
+        dollar1: dollar1,
+        dollar2: dollar2,
+        price: price,
+        tvl: tvl,
+        solana: solana,
+        solana1: solana1,
+        solana2: solana2,
+        solana3: solana3,
+        eth: eth,
+        eth1: eth1,
+        eth2: eth2,
+        eth3: eth3,
+        eth4: eth4,
+        eth5: eth5,
+        eth6: eth6,
+        binance2: binance2,
+        binance: binance,
+        binance1: binance1,
+        pp : pp
+    },
+    {
+        dollargreen:dollargreen,
+        bitcoinlogo:bitcoinlogo,
+        bitcointxt:bitcointxt,
+        dollar1: dollar1,
+        dollar2: dollar2,
+        price: price,
+        tvl: tvl,
+        solana: solana,
+        solana1: solana1,
+        solana2: solana2,
+        solana3: solana3,
+        eth: eth,
+        eth1: eth1,
+        eth2: eth2,
+        eth3: eth3,
+        eth4: eth4,
+        eth5: eth5,
+        eth6: eth6,
+        binance2: binance2,
+        binance: binance,
+        binance1: binance1,
+        pp : pp
+    },
+]
+
+export default data
